9b1d2c6f078daee6e001db8ddb0e64e44ba60165
Roman Tymchenko <romatimchenko1984@gmail.com>
2019-10-12 11:56:40 +0300

    feat: add solution of task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+    // we understand, in what square is our searching target
     guess() {
-
+        return this.mid = Math.round((this.min + this.max) / 2);
     }
-
+    // if searching target is lower ot greater than middle, we assign different value
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
+        this.min = this.mid;
 
     }
 }
